b85b85d9bf33237c4c11a40dbca05c725f93793b
nimarchik <lexaxiip@gmail.com>
2025-07-08 21:41:57 +0300

    edit helmet

--- src/Page/Helmets.jsx
+++ src/Page/Helmets.jsx
@@ -4,14 +4,31 @@ import { useTranslation } from 'react-i18next';
 
 const Helmets = ({ titleKey = 'meta.title', descKey = 'meta.description' }) => {
   const { t } = useTranslation()
+
   return <Helmet>
     <title>{t(titleKey)}</title>
     <meta name="description" content={t(descKey)} />
     <meta name="keywords" content={t('keywords')} />
     <meta property="og:title" content="GGenius" />
     <meta property="og:description" content={t('descript')} />
-    <meta property="og:image" content="https://example.com/preview.jpg" />
+    <meta property="og:image" content="https://ggenius.gg/preview.jpg" />
     <meta name="google-site-verification" content="e8cZ7JXTTYmpoCGCTEgfCeYSvBHT3yiFmNJLYv7nY_Y" />
+
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://ggenius.gg/" />
+    <meta property="og:title" content="GGenius" />
+    <meta property="og:description" content={t('homeWrapperSub')} />
+    <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://ggenius.gg/" />
+    <meta property="twitter:title" content="GGenius" />
+    <meta property="twitter:description" content={t('homeWrapperSub')} />
+    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+
   </Helmet>
 }
 
