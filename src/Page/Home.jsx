bc3a10f10320dc3fd18feee70635745c7b819574
nimarchik <lexaxiip@gmail.com>
2025-07-09 20:35:30 +0300

    fix seo2

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,7 @@
 
 import style from '../assets/style/index.module.css'
 
-import ggenius from '../assets/pictures/Home/ggenius.jpg'
+import ggenius from '../assets/pictures/Home/ggenius.webp'
 import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { useEffect } from 'react';
@@ -77,52 +77,58 @@ const Home = () => {
           </div>
 
           <ul className={style.worksWrapperList}>
-            <div data-aos="fade-right">
-              <li className={style.worksWrapperListItem}>
-                <svg className={style.worksWrapperListItemImg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                </svg>
-                <div className={style.worksWrapperListItemCont}>
-                  <h3 className={style.worksWrapperListItemTitle}>
-                    {t('Register')}
-                  </h3>
-                  <p className={style.worksWrapperListItemSub}>
-                    {t('RegisterSub')}
-                  </p>
+            <li className={style.worksWrapperListItem}>
+              <div data-aos="fade-right">
+                <div className={style.worksWrapperListItemGroups}>
+                  <svg className={style.worksWrapperListItemImg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                  </svg>
+                  <div className={style.worksWrapperListItemCont}>
+                    <h3 className={style.worksWrapperListItemTitle}>
+                      {t('Register')}
+                    </h3>
+                    <p className={style.worksWrapperListItemSub}>
+                      {t('RegisterSub')}
+                    </p>
+                  </div>
                 </div>
-              </li>
-            </div>
+              </div>
+            </li>
 
             <li className={style.worksWrapperListItem}>
               <div data-aos="fade-left">
-                <div className={style.worksWrapperListItemCont}>
+                <div className={style.worksWrapperListItemGroups}>
+                  <div className={style.worksWrapperListItemCont}>
 
-                  <h3 className={style.worksWrapperListItemTitle}>
-                    {t('AI')}
-                  </h3>
-                  <p className={style.worksWrapperListItemSub}>
-                    {t('AISub')}
-                  </p>
+                    <h3 className={style.worksWrapperListItemTitle}>
+                      {t('AI')}
+                    </h3>
+                    <p className={style.worksWrapperListItemSub}>
+                      {t('AISub')}
+                    </p>
+                  </div>
+                  <svg className={style.worksWrapperListItemImg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5M3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.6 26.6 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.93.93 0 0 1-.765.935c-.845.147-2.34.346-4.235.346s-3.39-.2-4.235-.346A.93.93 0 0 1 3 9.219zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a25 25 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25 25 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135" />
+                    <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2zM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5" />
+                  </svg>
                 </div>
-                <svg className={style.worksWrapperListItemImg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5M3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.6 26.6 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.93.93 0 0 1-.765.935c-.845.147-2.34.346-4.235.346s-3.39-.2-4.235-.346A.93.93 0 0 1 3 9.219zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a25 25 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25 25 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135" />
-                  <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2zM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5" />
-                </svg>
               </div>
             </li>
 
             <li className={style.worksWrapperListItem}>
               <div data-aos="fade-right">
-                <svg className={style.worksWrapperListItemImg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5q0 .807-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33 33 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935m10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
-                </svg>
-                <div className={style.worksWrapperListItemCont}>
-                  <h3 className={style.worksWrapperListItemTitle}>
-                    {t('Tournament')}
-                  </h3>
-                  <p className={style.worksWrapperListItemSub}>
-                    {t('TournamentSub')}
-                  </p>
+                <div className={style.worksWrapperListItemGroups}>
+                  <svg className={style.worksWrapperListItemImg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5q0 .807-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33 33 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935m10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
+                  </svg>
+                  <div className={style.worksWrapperListItemCont}>
+                    <h3 className={style.worksWrapperListItemTitle}>
+                      {t('Tournament')}
+                    </h3>
+                    <p className={style.worksWrapperListItemSub}>
+                      {t('TournamentSub')}
+                    </p>
+                  </div>
                 </div>
               </div>
             </li>
