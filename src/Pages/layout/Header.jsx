a5bfc14c2b32b459222b02486d364173847520ee
nimarchik <lexaxiip@gmail.com>
2025-07-10 20:31:14 +0300

    fix sitemap

--- src/Pages/layout/Header.jsx
+++ src/Pages/layout/Header.jsx
@@ -50,68 +50,74 @@ const Header = () => {
 
   return <>
     <header className={style.header}>
-      <div className={style.container}>
-        <nav className={style.nav}>
-          <Link to={`/${lang}/Home`} className={style.navLogo} aria-label='link to the Home'>
-            <svg className={style.logoSvg} xmlns="http:;//www.w3.org/2000/svg" width="326" height="99" viewBox="0 0 326 99">
-              <text className={style.logoSvg} id="GGenius" xmlSpace="preserve" x="3.148438" y="77" fontFamily="Sansation" fontSize="70" fill="gold">GGenius</text>
-            </svg>
-          </Link>
+      <div data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="500">
+        <div className={style.container}>
 
-          <div className={!menu ? style.navLinksListActive : style.navLinksList}>
-            <ul className={style.navList}>
-              <li className={style.navListItem}>
-                <Link aria-label='link to the Home' to={`/${lang}/Home`} onClick={() => setMenu(!menu)} className={currentPath.endsWith('/Home') ? style.navListItemLinkActive : style.navListItemLink}>
-                  {t('home')}
-                </Link>
-              </li>
-              <li className={style.navListItem}>
-                {/* to={`/${lang}/Heroes`} */}
-                <Link aria-label='link to the heroes page' onClick={e => e.preventDefault()} className={currentPath.endsWith('Heroes') ? style.navListItemLinkActive : style.navListItemLink}>
-                  {t('heroes')}
-                </Link>
-              </li>
-              <li className={style.navListItem}>
-                <Link aria-label='link to the Contact page' onClick={e => e.preventDefault()} className={style.navListItemLink}>
-                  {t('Notice')}
-                </Link>
-              </li>
-              <li className={style.navListItem}>
-                <Link aria-label='link to the Guides page' onClick={e => e.preventDefault()} className={style.navListItemLink}>
-                  {t('Guides')}
-                </Link>
-              </li>
-              <li className={style.navListItem}>
-                <Link aria-label='link to the Esports page' onClick={e => e.preventDefault()} className={style.navListItemLink}>
-                  {t('Esports')}
-                </Link>
-              </li>
-              <li className={style.navListItem}>
-                <Link aria-label='link to the About page' to={`/${lang}/About`} onClick={() => setMenu(!menu)} className={currentPath.endsWith('About') ? style.navListItemLinkActive : style.navListItemLink}>
-                  {t('about')}
-                </Link>
-              </li>
-            </ul>
-            <div className={style.navGroups}>
-              <select className={style.navLang} value={value} onChange={e => changeLangukge(e.target.value)}>
-                <option value="uk">UK</option>
-                <option value="en">EN</option>
-              </select>
+          <nav className={style.nav}>
+            <Link to={`/${lang}/Home`} className={style.navLogo} aria-label='link to the Home'>
+              <svg className={style.logoSvg} xmlns="http:;//www.w3.org/2000/svg" width="326" height="99" viewBox="0 0 326 99">
+                <text className={style.logoSvg} id="GGenius" xmlSpace="preserve" x="3.148438" y="77" fontFamily="Sansation" fontSize="70" fill="gold">GGenius</text>
+              </svg>
+            </Link>
 
-              <Link aria-label='link to the login page' className={style.navLogin}>
-                {t('Login')}
-              </Link>
+            <div className={!menu ? style.navLinksListActive : style.navLinksList}>
+              <ul className={style.navList}>
+                <li className={style.navListItem}>
+                  <Link aria-label='link to the Home' to={`/${lang}/Home`} onClick={() => setMenu(!menu)} className={currentPath.endsWith('/Home') ? style.navListItemLinkActive : style.navListItemLink}>
+                    {t('home')}
+                  </Link>
+                </li>
+                <li className={style.navListItem}>
+                  {/* to={`/${lang}/Heroes`} */}
+                  <Link aria-label='link to the heroes page' onClick={e => e.preventDefault()} className={currentPath.endsWith('Heroes') ? style.navListItemLinkActive : style.navListItemLink}>
+                    {t('heroes')}
+                  </Link>
+                </li>
+                <li className={style.navListItem}>
+                  <Link aria-label='link to the Contact page' onClick={e => e.preventDefault()} className={style.navListItemLink}>
+                    {t('Notice')}
+                  </Link>
+                </li>
+                <li className={style.navListItem}>
+                  <Link aria-label='link to the Guides page' onClick={e => e.preventDefault()} className={style.navListItemLink}>
+                    {t('Guides')}
+                  </Link>
+                </li>
+                <li className={style.navListItem}>
+                  <Link aria-label='link to the Esports page' onClick={e => e.preventDefault()} className={style.navListItemLink}>
+                    {t('Esports')}
+                  </Link>
+                </li>
+                <li className={style.navListItem}>
+                  <Link aria-label='link to the About page' to={`/${lang}/About`} onClick={() => setMenu(!menu)} className={currentPath.endsWith('About') ? style.navListItemLinkActive : style.navListItemLink}>
+                    {t('about')}
+                  </Link>
+                </li>
+              </ul>
+              <div className={style.navGroups}>
+                <select className={style.navLang} value={value} onChange={e => changeLangukge(e.target.value)}>
+                  <option value="uk">UK</option>
+                  <option value="en">EN</option>
+                </select>
+
+                <Link aria-label='link to the login page' className={style.navLogin}>
+                  {t('Login')}
+                </Link>
+              </div>
             </div>
-          </div>
 
 
 
-          <button title='Menu' aria-label='Button Menu' className={style.navMenu} onClick={() => setMenu(!menu)}>
-            <span className={menu ? style.navMenuSpan1Active : style.navMenuSpan1}></span>
-            <span className={menu ? style.navMenuSpan2Active : style.navMenuSpan2}></span>
-            <span className={menu ? style.navMenuSpan3Active : style.navMenuSpan3}></span>
-          </button>
-        </nav>
+            <button title='Menu' aria-label='Button Menu' className={style.navMenu} onClick={() => setMenu(!menu)}>
+              <span className={menu ? style.navMenuSpan1Active : style.navMenuSpan1}></span>
+              <span className={menu ? style.navMenuSpan2Active : style.navMenuSpan2}></span>
+              <span className={menu ? style.navMenuSpan3Active : style.navMenuSpan3}></span>
+            </button>
+          </nav>
+
+        </div>
       </div>
     </header>
   </>;
