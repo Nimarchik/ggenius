c7a7546da2496937e9ff65f455bbe91b1bf371c8
nimarchik <lexaxiip@gmail.com>
2025-07-16 00:04:27 +0300

    fix helm

--- src/Pages/Helmets.jsx
+++ src/Pages/Helmets.jsx
@@ -32,6 +32,21 @@ GGenius підкаже кращих героїв, ефективні збірк�
     <meta property="twitter:image" content="https://ggenius.gg/logo.webp" />
     <link rel="icon" href="/preview.webp" />
 
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
+      rel="stylesheet"
+    />
+
+  
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="GGenius" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
   </Helmet>
 }
 
