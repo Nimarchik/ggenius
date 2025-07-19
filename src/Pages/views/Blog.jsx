3e84d8d02fa1711e28386176464574480e234842
nimarchik <lexaxiip@gmail.com>
2025-07-19 20:45:15 +0300

    fix blogs

--- src/Pages/views/Blog.jsx
+++ src/Pages/views/Blog.jsx
@@ -1,14 +1,17 @@
 import { Logger } from 'sass';
 import style from '../../assets/style/index.module.css'
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import Helmets from '../Helmets';
+import useTranslatedList from '../../hooks/useTranslatedList';
+
 
 
 const Blog = () => {
   const { t } = useTranslation();
   const [show, setShow] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   const [patches, setPatches] = useState([]);
 
@@ -19,15 +22,55 @@ const Blog = () => {
       .then(res => res.json())
       .then(data => {
         setPatches(data);
-        // console.log(data);
+        setLoading(false);
       })
       .catch(err => {
         console.error('Ошибка загрузки патчей:', err);
       });
   }, []);
 
+
+  const langCode = localStorage.getItem('value')
+
+  function useTranslatedMap(list, lang) {
+    const normalizedList = useMemo(() => {
+      if (!list) return [];
+      return list.filter(Boolean);
+    }, [list]);
+
+    const translatedList = useTranslatedList(normalizedList, lang);
+
+    return useMemo(() => {
+      const map = {};
+      normalizedList.forEach((item, i) => {
+        map[item] = translatedList[i];
+      });
+      return map;
+    }, [normalizedList, translatedList]);
+  }
+
+  // Получаем списки для перевода
+  const title = useMemo(() => patches?.map(s => s?.title) || [], [patches]);
+  const des = useMemo(() => patches?.map(s => s?.content) || [], [patches]);
+
+  // Получаем мапы переводов
+  const translatedMap = useTranslatedMap(title, langCode);
+  const translatedMapDes = useTranslatedMap(des, langCode);
+
+
+
+
+
+
+ if (loading) return <div className={style.loading}>
+    <div className={style.spin}>
+      <span className={style.spinSpan}></span>
+    </div>
+  </div>
+
   return <>
     <Helmets titleKey='Blog.title' descKey="Blog.description" />
+
     <section className={style.blog}>
       <div className={style.container}>
         <div className={style.blogWrapper}>
@@ -42,16 +85,16 @@ const Blog = () => {
                     <div className={style.blogWrapperPatchesInnerGroup}>
                       <img className={style.blogWrapperPatchesInnerGroupImg} src={item.image} alt="" />
                       <div className={style.blogWrapperPatchesInnerGroupText}>
-                        <h1 className={style.blogWrapperPatchesInnerTitle}>{item.title}</h1>
+                        <h1 className={style.blogWrapperPatchesInnerTitle}>{translatedMap[item.title] || item.title}</h1>
                         <p className={style.blogWrapperPatchesInnerGroupTextDate}>
                           {item.date}
                         </p>
                       </div>
                       <p className={show ? style.blogWrapperPatchesInnerGroupTimeActive : style.blogWrapperPatchesInnerGroupTime}>
-                        {item.content}
+                        {translatedMapDes[item.content] || item.content}
                       </p>
                       <p className={style.blogWrapperPatchesInnerGroupShow}>
-                        {show ? 'Згорнути ⬆️' : 'Розгорнути ⬇️'}
+                        {show ? t('hidden') : t('show')}
                       </p>
                     </div>
                     <p className={style.blogWrapperPatchesInnerSub}>
